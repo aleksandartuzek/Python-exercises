781294e30a2374cf4e3971d3757c655c2bc84e7c
aleksandartuzek <tuzek94@gmail.com>
2020-07-26 17:18:35 +0200

    Swap two variables

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#Calculate the average
+
+Geometry = int(input("Input score from Geometry: "))
+Algebra = int(input("Input score Algebra: "))
+Physics = int(input("Input score Physics: "))
+
+average_score = (Geometry + Algebra + Physics) / 3
+
+print (average_score)
